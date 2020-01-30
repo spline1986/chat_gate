90e3ccfd7ece36ba0984a9dc0693f78dea0a4b8c
Andrew Lobanov <spline@CobraMK3>
2020-01-30 21:49:42 +0500

    Немного причесал код.

--- tg_gate.py
+++ tg_gate.py
@@ -12,17 +12,20 @@ bot = telebot.TeleBot(cfg["token"])
 
 
 def create_fifo(path):
+    "Create FIFO file by path."
     if os.path.exists(path):
         os.unlink(path)
     if not os.path.exists(path):
         os.mkfifo(path)
 
 def write(s):
+    "Write s into output FIFO."
     with open(cfg["out"], "w") as write_fifo:
         write_fifo.write(s + "\n")
 
 
-class MyThread(Thread):
+class ListenerThread(Thread):
+    "Input FIFO listener."
     def __init__(self, name):
         Thread.__init__(self)
         self.name = name
@@ -38,6 +41,7 @@ class MyThread(Thread):
 
 @bot.message_handler(content_types=['text'])
 def get_text(message):
+    "Write incoming message into out FIFO."
     user = "{} {}".format(message.from_user.first_name,
                           message.from_user.last_name)
     write("<{}>: {}".format(user.strip(), message.text))
@@ -45,6 +49,7 @@ def get_text(message):
 
 @bot.message_handler(content_types=["audio", "document", "photo"])
 def get_photo(message):
+    "Write incoming file into files/ directory."
     if message.photo:
         file_id = message.photo[0].file_id
     elif message.document:
@@ -54,11 +59,11 @@ def get_photo(message):
     file_info = bot.get_file(file_id)
     ext = file_info.file_path.split(".")[-1]
     f = bot.download_file(file_info.file_path)
-    open(file_id + "." + ext, "wb").write(f)
+    open("files/{}.{}".format(file_id, ext), "wb").write(f)
 
 
 create_fifo(cfg["out"])
 create_fifo(cfg["in"])
-my_thread = MyThread("Telegram fifo listener")
-my_thread.start()
+listener_thread = ListenerThread("Telegram fifo listener")
+listener_thread.start()
 bot.polling()
